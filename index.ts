b0525a785d49f9bffaccd0b8a590d3456ee291c6
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-19 20:17:19 -0300

    tabata refresh

--- index.ts
+++ index.ts
@@ -8,9 +8,12 @@ let desc: number = 11;
 let ciclo: number = 8;
 let work: number = 21;
 
-
-
 let intervalId = setInterval(() => {
   desc = desc - 1;
-  document.getElementById('app.counter').innerHTML = desc.toString();
+  document.getElementById('app.desc').innerHTML = desc.toString();
+}, 1000);
+
+let intervalId2 = setInterval(() => {
+  work = work - 1;
+  document.getElementById('app.work').innerHTML = work.toString();
 }, 1000);
