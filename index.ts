8be1253889ca362d82f9ca06c5343c0b1068e670
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-19 19:58:04 -0300

    Montando um contador

--- index.ts
+++ index.ts
@@ -2,19 +2,11 @@
 import './style.css';
 
 // Write TypeScript code!
-let title: string = 'MyApp';
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>${title}</h1>
-<hr/>
-<p>Paragrafo qualquer</p>
-`;
-//exibindo no console
-console.log('Olá mundo');
+let title: string = 'My Counter';
+document.getElementById('app.title').innerHTML = title;
+let counter: number = 0;
 
-//sintaxe do for
-for (let i = 1; i <= 10; i++) {
-  //sintaxer do if
-  if (i % 2 == 0) {
-    console.log(i);
-  }
-}
+let intervalId = setInterval(() => {
+  counter = counter + 1;
+  document.getElementById('app.counter').innerHTML = counter.toString();
+}, 1000);
