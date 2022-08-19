cb362433cd0c9605f1d877db07751faae42f1e27
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-19 19:43:37 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -2,14 +2,18 @@
 import './style.css';
 
 // Write TypeScript code!
+let title: string = 'MyApp';
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-
+appDiv.innerHTML = `<h1>${title}</h1>
+<hr/>
+<p>Paragrafo qualquer</p>
+`;
 //exibindo no console
 console.log('Olá mundo');
 
 //sintaxe do for
 for (let i = 1; i <= 10; i++) {
+  //sintaxer do if
   if (i % 2 == 0) {
     console.log(i);
   }
