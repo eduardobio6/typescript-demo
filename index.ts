242907937aeae478fa6248399020b8ba10d68a8c
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-19 20:12:41 -0300

    tabata

--- index.ts
+++ index.ts
@@ -4,9 +4,13 @@ import './style.css';
 // Write TypeScript code!
 let title: string = 'My Counter';
 document.getElementById('app.title').innerHTML = title;
-let counter: number = 0;
+let desc: number = 11;
+let ciclo: number = 8;
+let work: number = 21;
+
+
 
 let intervalId = setInterval(() => {
-  counter = counter + 1;
-  document.getElementById('app.counter').innerHTML = counter.toString();
+  desc = desc - 1;
+  document.getElementById('app.counter').innerHTML = desc.toString();
 }, 1000);
