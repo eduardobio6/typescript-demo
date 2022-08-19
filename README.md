6fe4429bf5b962546df47e4103cb4bec9eb568c0
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-19 19:32:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-o7puyo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-o7puyo)
---
+++ index.ts
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+//exibindo no console
+console.log('Olá mundo');
+
+//sintaxe do for
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
