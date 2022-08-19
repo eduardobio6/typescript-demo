b00680be1d9d8b591a478863c0dc99fdf6713a77
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-19 20:44:00 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -2,16 +2,22 @@
 import './style.css';
 
 // Write TypeScript code!
-let title: string = 'My Counter';
-document.getElementById('app.title').innerHTML = title;
-let desc: number = 11;
+/*let title: string = 'My Counter';
+document.getElementById('app.title').innerHTML = title;*/
+let desc: number = 0;
 let ciclo: number = 8;
 let work: number = 21;
 
-let intervalId = setInterval(() => {
-  desc = desc - 1;
-  document.getElementById('app.desc').innerHTML = desc.toString();
-}, 1000);
+for (let i = 0; i < 1; i++) {
+  let intervalId = setInterval(() => {
+    desc = desc + 1;
+    document.getElementById('app.desc').innerHTML = desc.toString();
+  }, 1000);
+
+  if (i == 10) {
+    break;
+  }
+}
 
 let intervalId2 = setInterval(() => {
   work = work - 1;
