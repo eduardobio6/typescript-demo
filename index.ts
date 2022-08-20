a560ea3faf56152db8ed11753919035e27827b76
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-19 21:06:03 -0300

    tataba refresh 2

--- index.ts
+++ index.ts
@@ -8,18 +8,16 @@ let desc: number = 0;
 let ciclo: number = 8;
 let work: number = 21;
 
-for (let i = 0; i < 1; i++) {
+for (let i = 0; i <= 1; i++) {
   let intervalId = setInterval(() => {
-    desc = desc + 1;
-    document.getElementById('app.desc').innerHTML = desc.toString();
+    desc = desc + i;
+    if (desc <= 10) {
+      document.getElementById('app.desc').innerHTML = desc.toString();
+    }
   }, 1000);
-
-  if (i == 10) {
-    break;
-  }
 }
 
-let intervalId2 = setInterval(() => {
+/*let intervalId2 = setInterval(() => {
   work = work - 1;
   document.getElementById('app.work').innerHTML = work.toString();
-}, 1000);
+}, 1000);*/
