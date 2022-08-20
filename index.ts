dc2e271fbf5075dd53bf88215730ec61b29a4881
eduardobio6 <eduardooliveira278@gmail.com>
2022-08-19 22:24:11 -0300

    tentei

--- index.ts
+++ index.ts
@@ -5,19 +5,64 @@ import './style.css';
 /*let title: string = 'My Counter';
 document.getElementById('app.title').innerHTML = title;*/
 let desc: number = 0;
-let ciclo: number = 8;
-let work: number = 21;
+let ciclo: number = 0;
+let work: number = 0;
+let fim: string = 'Finalizado';
 
-for (let i = 0; i <= 1; i++) {
+/*for (let i = 0; i <= 1; i++) {
   let intervalId = setInterval(() => {
-    desc = desc + i;
-    if (desc <= 10) {
-      document.getElementById('app.desc').innerHTML = desc.toString();
+    work = work + i;
+    document.getElementById('app.work').innerHTML = work.toString();
+
+    if (work == 5) {
+      work = 0;
     }
-  }, 1000);
-}
+    }, 1000);
+
+    let intervalId2 = setInterval(() => {
+      desc = desc + i;
+      document.getElementById('app.desc').innerHTML = desc.toString();
+
+      if(desc == 2){
+        desc = 0;
+        ciclo++;
+        document.getElementById('app.ciclo').innerHTML = ciclo.toString();           
+        if (ciclo == 8) {
+        document.getElementById('app.finish').innerHTML = fim.toString();
+      }
+      }
+      }, 1000);
+}*/
+let intervalId2 = setInterval(() => {
+  work = work + 1;
+  document.getElementById('app.work').innerHTML = work.toString();
+  if (work == 20) {
+    work = 0;
+  }
+}, 1000);
+
+let intervalId = setInterval(() => {
+  desc = desc + 1;
+  document.getElementById('app.desc').innerHTML = desc.toString();
+
+  if (desc == 10) {
+    desc = 0;
+    ciclo = ciclo + 1;
+    document.getElementById('app.ciclo').innerHTML = ciclo.toString();
+
+    if (ciclo == 8) {
+      ciclo = 0;
+    }
+  }
+}, 1000);
 
 /*let intervalId2 = setInterval(() => {
   work = work - 1;
   document.getElementById('app.work').innerHTML = work.toString();
-}, 1000);*/
+}, 1000);
+
+ if (desc <= 10) {
+      document.getElementById('app.desc').innerHTML = desc.toString();
+    }
+
+*/
